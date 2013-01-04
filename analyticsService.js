0f1a32b46607951207494bf5f3ca50aea44f84ee
Ido Samuelson <ido.samuelson@gmail.com>
2013-01-04 12:20:42 -0800

    Update analyticsService.js
    Convert path route params to query string
    
    route of /account/:acountId which results in /account/123
    will now report to google analytics the following path:
    
    /account?accountId=123

--- analyticsService.js
+++ analyticsService.js
@@ -1,4 +1,3 @@
-
 var _gaq = _gaq || [];
 
 angular.module('analytics', []).run(['$http', function($http) {
@@ -11,9 +10,25 @@ angular.module('analytics', []).run(['$http', function($http) {
 	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(ga, s);
 
-}]).service('analytics', function($rootScope, $window, $location) {
-	var track = function() {
-		$window._gaq.push(['_trackPageview', $location.path()]);
-	};
+}]).service('analytics', function($rootScope, $window, $location, $routeParams) {
+
 	$rootScope.$on('$viewContentLoaded', track);
+
+	var track = function() {
+		var path = convertPathToQueryString($location.path(), $routeParams)
+		$window._gaq.push(['_trackPageview', path]);
+	};
+	
+	var convertPathToQueryString = function(path, $routeParams) {
+		for (var key in $routeParams) {
+			var queryParam = '/' + $routeParams[key];
+			path = path.replace(queryParam, '');
+		}
+
+		var querystring = decodeURIComponent($.param($routeParams));
+
+		if (querystring === '') return path;
+
+		return path + "?" + querystring;
+	};
 });
